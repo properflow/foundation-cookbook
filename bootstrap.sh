f9da70839f4eb916540065c53428a21b0027f146
Dylan Vaughn <dylancvaughn@gmail.com>
2013-06-29 17:00:19 -0700

    wip bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,3 @@ unzip master.zip
 
 # Install AWS EC2 API tools & Java via bootstrap recipe
 /opt/chef/bin/chef-solo -j /root/foundation-cookbook-master/node.json -c /root/foundation-cookbook-master/solo.rb
-
-# update PATH
-# TODO: don't hard-code ec2-api-tools version
-echo 'PATH="/apps/aws/ec2-api-tools-1.6.7.4/bin:/usr/local/aws/ec2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"' > /etc/environment
-
--- site-cookbooks/netflix/recipes/foundation-builder.rb
+++ site-cookbooks/netflix/recipes/foundation-builder.rb
@@ -9,15 +9,15 @@
 
 # disable i386
 file "/etc/dpkg/dpkg.cfg.d/multiarch" do
-    action :delete
+  action :delete
 end
 
 # didn't want to install the apt lwrp
 bash "apt-get update" do
-    user "root"
-    code <<-EOH
-        apt-get update
-    EOH
+  user "root"
+  code <<-EOH
+apt-get update
+EOH
 end
 
 package "openjdk-7-jdk"
@@ -25,15 +25,15 @@ package "unzip"
 
 # TODO do an unless exists check here
 user "ubuntu" do
-    home "/home/ubuntu"
-    shell "/bin/bash"
-    supports :manage_home => true
+  home "/home/ubuntu"
+  shell "/bin/bash"
+  supports :manage_home => true
 end
 
 remote_file "/tmp/ec2-api-tools.zip" do
-    source "http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip"
-    owner "ubuntu"
-    mode "0755"
+  source "http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip"
+  owner "ubuntu"
+  mode "0755"
 end
 
 directory "/apps/aws" do
@@ -45,9 +45,9 @@ directory "/apps/aws" do
 end
 
 bash "unzip ec2-api-tools" do
-    user "root"
-    cwd "/apps/aws"
-    code <<-EOH
+  user "root"
+  cwd "/apps/aws"
+  code <<-EOH
     if [ ! -d /tmp/ec2-api-tools.zip ]
     then
         unzip -o /tmp/ec2-api-tools.zip
@@ -55,11 +55,13 @@ bash "unzip ec2-api-tools" do
     EOH
 end
 
-bash "update bashrc" do
-    user "root"
-    cwd "/etc/skel"
-    code <<-EOH
-        echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre" >> .bashrc
-        echo "export EC2_HOME=/apps/aws/ec2-api-tools-1.6.6.4" >> .bashrc
-    EOH
+file "/etc/environment" do
+  owner "root"
+  group "root"
+  mode "0644"
+  content <<-EOH
+PATH="/apps/aws/ec2-api-tools-1.6.7.4/bin:/usr/local/aws/ec2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+EC2_HOME=/apps/aws/ec2-api-tools-1.6.7.4
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
+EOH
 end
